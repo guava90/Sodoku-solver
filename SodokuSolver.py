469466cda91978262ae5db1fc0f59736ea455bdb
Mattias Johan Oskar Strömblad Lindh <38034997+guava90@users.noreply.github.com>
2018-04-04 18:15:43 +0200

    Update SodokuSolver.py

--- SodokuSolver.py
+++ SodokuSolver.py
@@ -1,17 +1,5 @@
 # Pathway: C:\Users\Mattias\AppData\Local\Programs\Python\Python36-32
 
-# Hårdkodad Sodoku att testa med. Går att göra egen med funktionen setup fast
-# det är ganska jobbigt. Vill göra en smidigare inmatning där man skriver in
-# allt på en gång.
-sodoku25 = [[[" "],[" "],[" "],[3],[" "],[" "],[" "],[" "],[" "]],
-          [[" "],[" "],[" "],[" "],[" "],[" "],[9],[8],[" "]],
-          [[" "],[" "],[2],[1],[4],[" "],[" "],[" "],[" "]],
-          [[" "],[" "],[" "],[" "],[" "],[" "],[" "],[1],[4]],
-          [[" "],[9],[" "],[" "],[" "],[" "],[" "],[3],[" "]],
-          [[6],[8],[" "],[" "],[" "],[" "],[" "],[" "],[" "]],
-          [[" "],[" "],[" "],[" "],[9],[8],[6],[" "],[" "]],
-          [[" "],[2],[1],[" "],[" "],[" "],[" "],[" "],[" "]],
-          [[" "],[" "],[" "],[" "],[" "],[5],[" "],[" "],[" "]]]
 # Delade upp koden i moduler när den började bli stor.
 # Ska kanske lägga till en module för vanligare funktioner som
 # write_number() i slice_n_dice.py utifall jag behöver den i
@@ -22,20 +10,15 @@ import slice_n_dice
 import bounded_pairs
 #import search_sodoku
         
-def setup(sodoku):    
-    insert = 1
-    while insert == 1:
-        print("Insert locked numbers.")
-        x = int(input("x koordinate: "))
-        y = int(input("y koordinate: "))
-        value = int(input("Enter the value: "))
-
-        insert_number(sodoku,value,x,y)
-        response = input("Input another value? y/n ")
-        if response == "n":
-            insert = 0
-
-
+sodoku25 = [[[" "],[" "],[" "],[3],[" "],[" "],[" "],[" "],[" "]],
+          [[" "],[" "],[" "],[" "],[" "],[" "],[9],[8],[" "]],
+          [[" "],[" "],[2],[1],[4],[" "],[" "],[" "],[" "]],
+          [[" "],[" "],[" "],[" "],[" "],[" "],[" "],[1],[4]],
+          [[" "],[9],[" "],[" "],[" "],[" "],[" "],[3],[" "]],
+          [[6],[8],[" "],[" "],[" "],[" "],[" "],[" "],[" "]],
+          [[" "],[" "],[" "],[" "],[9],[8],[6],[" "],[" "]],
+          [[" "],[2],[1],[" "],[" "],[" "],[" "],[" "],[" "]],
+          [[" "],[" "],[" "],[" "],[" "],[5],[" "],[" "],[" "]]]
 
 # TESTS
 
@@ -45,7 +28,7 @@ sodoku_list.make_list(sodoku25)
 empty = slice_n_dice.find_singles(sodoku25)
 empty = slice_n_dice.hidden_singles(sodoku25)
 print("Entering for-loop.")
-for i in range(11):
+for i in range(10):
     empty = slice_n_dice.find_singles(sodoku25)
     empty = slice_n_dice.hidden_singles(sodoku25)
 
@@ -59,8 +42,14 @@ for i in range(11):
     for k in range(len(koord)):
         if k % 2 == 0:
             sodoku_list.update_list_pair(sodoku25, koord[k], koord[k + 1])
-   
-
+    
+    if empty == 0:
+        print("Done after", i, "iterations!")
+        break
+    else:
+        print(100 * (1 - empty / 81), "% done.")
+    
+    
 print("Output:")
 print_sodoku.print_sodoku(sodoku25)
 
