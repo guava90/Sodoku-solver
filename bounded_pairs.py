ba6c2724ae2e73a5b925df0a54abff74f3ed5219
Mattias Johan Oskar Strömblad Lindh <38034997+guava90@users.noreply.github.com>
2018-04-04 07:41:31 +0200

    Update bounded_pairs.py
    Added a few functions and also descriptions. The function bounded_pair_box() isn't used at the moment and I'm not sure if I actually need it.

--- bounded_pairs.py
+++ bounded_pairs.py
@@ -1,18 +1,43 @@
 # Bounded pair module
 
 def find_pair(sodoku):
-    pair_koord = []
+    # Kollar vilka celler som har listor med två element + " ".
+    # Returnerar lista med koordinater.
+    pair_koord = [[],[]]
+    k = 0
     for i in range(9):
         for j in range(9):
             if len(sodoku[i][j]) == 3:
-                pair_koord.append([i,j])
+                pair_koord[0].append([i,j])
+                pair_koord[1].append(sodoku[i][j])
+                #pair_koord[1][k].remove(" ")
+                k = k + 1
     return pair_koord
 
-def bounded_pair_box(pair_koord):
+def bounded_pair_box(pair_koord): # ANDVÄNDS EJ I NULÄGET!
+    # Kollar vilka par som ligger i samma box.
     check = []
-    pair = []
-    for k in range(len(pair_koord)):
-        check.append([pair_koord[k][0] - pair_koord[k][0] % 3,
-                      pair_koord[k][1] - pair_koord[k][1] % 3])
-        # kolla om check innehåller par
+    for k in range(len(pair_koord[0])):
+        check.append([pair_koord[0][k][0] - pair_koord[0][k][0] % 3,
+                      pair_koord[0][k][1] - pair_koord[0][k][1] % 3])
+    return check
 
+def compare_pair(list):
+    # Kollar om paren i list upprepas och när.
+    pair = []
+    for n in range(len(list) - 1):
+        for k in range(n + 1, len(list)):
+            if list[n][1] == list[k][1] and list[n][2] == list[k][2]:
+                pair.append([n,k])
+    return pair
+    
+def bounded_pair(pair_koord):
+    # Tar fram bundna par ur pair_koord och ordnar deras koordinater
+    # och returnerar dom i par.
+    koord = []
+    pair = compare_pair(pair_koord[1])
+    for k in range(len(pair)):
+        for l in range(2):
+            koord.append(pair_koord[0][pair[k][l]])
+    return koord
+    
