0eed325a194c89d48e990a64f9bc083425b86af2
Mattias Johan Oskar Strömblad Lindh <38034997+guava90@users.noreply.github.com>
2018-04-18 17:00:01 +0200

    Update x_wing.py

--- x_wing.py
+++ x_wing.py
@@ -2,14 +2,17 @@
 import search_sodoku
 import bounded_pairs
 
-def x_search_row(sodoku, a):
+def x_search_row(sodoku, a, m = 2):
     # Söker igenom sodoku efter a och om a förekommer två gånger i två rader
     # returnerar funktionen vilka rader det förekom.
+    # Ex. R = [[i0,j00,j01],...,[in,jn0,jn,1]]
+    # Anges m söker den igenom efter a och om a förekommer mellan två och m
+    # gånger istället. Detta för att kunna göra Svärdfiken.
     R = []
     k = 0
     for i in range(9):
         n = search_sodoku.search_row_hidden(sodoku, i, a)
-        if n == 2:
+        if n >= 2 and n <= m:
             R.append([i])
             for j in range(9):
                 if sodoku[i][j].count(a) == 1:
@@ -17,14 +20,14 @@ def x_search_row(sodoku, a):
             k = k + 1
     return R
 
-def x_search_kol(sodoku, a):
+def x_search_kol(sodoku, a, m = 2):
     # Söker igenom sodoku efter a och om a förekommer två gånger i två rader
     # returnerar funktionen vilka rader det förekom.
     K = []
     k = 0
     for j in range(9):
         n = search_sodoku.search_kol_hidden(sodoku, j, a)
-        if n == 2:
+        if n >= 2 and n <= m:
             K.append([j])
             for i in range(9):
                 if sodoku[i][j].count(a) == 1:
@@ -54,6 +57,6 @@ def x_wing_kol(sodoku):
             j1 = K[0][0]
             j2 = K[1][0]
             print("Found X-wing at (", i1, ",", j1, "), (", i2, ",", j2, ")")
-            bounded_pairs.remove_pair_row(sodoku, a, i1, j1, b, i1, j2)
-            bounded_pairs.remove_pair_row(sodoku, a, i2, j1, b, i2, j2)
+            bounded_pairs.remove_pair_row(sodoku, a, i1, j1, 0, i1, j2)
+            bounded_pairs.remove_pair_row(sodoku, a, i2, j1, 0, i2, j2)
     return
