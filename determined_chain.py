4c90ddf5ef86066af526f334e9d87c94fbff0725
Mattias Johan Oskar Strömblad Lindh <38034997+guava90@users.noreply.github.com>
2018-04-18 16:59:28 +0200

    Update determined_chain.py

--- determined_chain.py
+++ determined_chain.py
@@ -9,7 +9,7 @@ def determined_rectangle(sodoku):
     # vara siffran som ska vara där.
     list = bounded_pairs.find_pair(sodoku)
     koord = bounded_pairs.bounded_pair(list)
-    
+
     # Detta stycke görs för att se till så de paren som hittas ligger i
     # samma rad/kolumn.
     n = 0
@@ -21,7 +21,7 @@ def determined_rectangle(sodoku):
     if n != 0:
         for k in range(n):
             koord.remove([9, 9])
-            
+
     # Letar efter koordinater som dycker upp två gånger i koord. Gör de det
     # utgör de ett hörn. Därefter letar vi igenom kolumnen/raden efter de
     # koordinaten där kandidaterna finns och då hittar resterande hörn.
@@ -33,18 +33,67 @@ def determined_rectangle(sodoku):
                 a = sodoku[i1][j1][1]
                 b = sodoku[i1][j1][2]
                 for i in range(9):
-                    if i != i1 and sodoku[i][j1].count(a) == sodoku[i][j1].count(b) == 1:
+                    if (i != i1 and sodoku[i][j1].count(a) == sodoku[i][j1].count(b) == 1):
                         i2 = i
                 for j in range(9):
                     if j != j1 and sodoku[i1][j].count(a) == sodoku[i1][j].count(b) == 1:
                         j2 = j
-                
+
                 # Om hörnet i2,j2 har mer än 3 kandidater och två av dessa är
                 # samma som i resterande hörn tas de kandidaterna bort.
-                if len(sodoku[i2][j2]) > 3 and sodoku[i2][j2].count(a) == sodoku[i2][j2].count(b) == 1:
+                if (len(sodoku[i2][j2]) > 3 and
+                sodoku[i2][j2].count(a) == sodoku[i2][j2].count(b) == 1):
                     print("Found determined rectangle at: (", i1, ",", j1, ") , (", i1, ",", j2, ") , (", i2, ",", j1, ") , (", i2, ",", j2, ")")
                     print("Executing sodoku[", i2, "][", j2, "].remove(", a, ")")
                     print("Executing sodoku[", i2, "][", j2, "].remove(", b, ")")
                     sodoku[i2][j2].remove(a)
                     sodoku[i2][j2].remove(b)
+    return
+
+def semi_determined_rectangle(sodoku):
+    list = bounded_pairs.find_pair(sodoku)
+    koord = bounded_pairs.bounded_pair(list)
+
+    n = 0
+    for k in range(len(koord) - 1):
+        if (k % 2 == 0 and koord[k][0] != koord[k + 1][0] and
+        koord[k][1] != koord[k + 1][1]):
+            koord[k] = [9, 9]
+            koord[k + 1] = [9, 9]
+            n = n + 2
+    if n != 0:
+        for k in range(n):
+            koord.remove([9, 9])
+
+    for k in range(len(koord)):
+        if k % 2 == 0 and koord[k][0] == koord[k + 1][0]:
+            i1 = koord[k][0]
+            j1 = koord[k][1]
+            j2 = koord[k + 1][1]
+            a = sodoku[i1][j1][1]
+            b = sodoku[i1][j1][2]
+            for i in range(9):
+                if (i != i1 and sodoku[i][j1].count(a) == 1 and
+                sodoku[i][j1].count(b) == 1 and sodoku[i][j2].count(a) == 1 and
+                sodoku[i][j2].count(b) == 1 and
+                len(sodoku[i][j1]) == len(sodoku[i][j2]) == 4 and
+                sodoku[i][j1][3] == sodoku[i][j2][3]):
+                    c = sodoku[i][j1][3]
+                    print("Found semi determined rectangle.")
+                    bounded_pairs.remove_pair_row(sodoku, c, i, j1, 0, i, j2)
+        elif k % 2 == 0 and koord[k][1] == koord[k + 1][1]:
+            i1 = koord[k][0]
+            i2 = koord[k + 1][0]
+            j1 = koord[k][1]
+            a = sodoku[i1][j1][1]
+            b = sodoku[i1][j1][2]
+            for j in range(9):
+                if (j != j1 and sodoku[i1][j].count(a) == sodoku[i2][j].count(a)
+                == sodoku[i1][j].count(b) == sodoku[i2][j].count(b) == 1 and
+                len(sodoku[i1][j]) == len(sodoku[i2][j]) == 4 and
+                sodoku[i1][j][3] == sodoku[i2][j][3]):
+                    c = sodoku[i][j1][3]
+                    print("Found semi determined rectangle.")
+                    bounded_pairs.remove_pair_kol(sodoku, c, i1, j, 0, i2, j)
+
     return
